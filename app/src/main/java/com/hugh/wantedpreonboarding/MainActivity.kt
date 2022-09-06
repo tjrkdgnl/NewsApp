f4e8cd96916f5c783228aa454a577740f9b4804b
tjrkdgnl <skh0818@hanyang.ac.kr>
2022-09-07 00:30:28 +0900

    카테고리 프래그먼트 항목 안나오는 문제 해결

--- app/src/main/java/com/hugh/wantedpreonboarding/MainActivity.kt
+++ app/src/main/java/com/hugh/wantedpreonboarding/MainActivity.kt
@@ -1,70 +1,38 @@
 package com.hugh.wantedpreonboarding
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavHost
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationBarView
 import com.hugh.base.BaseActivity
 import com.hugh.wantedpreonboarding.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity :
-    BaseActivity<ActivityMainBinding>(R.layout.activity_main),
-    NavigationBarView.OnItemSelectedListener {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    private lateinit var navHost: NavHost
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setUpNavigation()
+        setUpBottomNavigationView()
         initToolbar()
     }
 
-    private fun setUpNavigation() {
-        val navHost =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    private fun setUpBottomNavigationView() {
+        navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         binding.bottomNavi.setupWithNavController(navHost.navController)
-
-        binding.bottomNavi.setOnItemSelectedListener(this)
     }
 
     private fun initToolbar() {
-        setToolbarTitle(binding.bottomNavi.selectedItemId)
-        setSupportActionBar(binding.toolbar)
-
         binding.toolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_700))
-        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navHost.navController)
     }
 
-    private fun setToolbarTitle(id: Int) {
-        when (id) {
-            R.id.headLine -> {
-                binding.toolbar.title = getString(R.string.head_line)
-            }
-            R.id.category -> {
-                binding.toolbar.title = getString(R.string.category)
-            }
-            R.id.bookmark -> {
-                binding.toolbar.title = getString(R.string.bookmark)
-            }
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.headLine -> {
-                setToolbarTitle(R.id.headLine)
-            }
-            R.id.category -> {
-                setToolbarTitle(R.id.category)
-            }
-            R.id.bookmark -> {
-                setToolbarTitle(R.id.bookmark)
-            }
-        }
-
-        return true
+    override fun onSupportNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- features/category/src/main/java/com/hugh/category/presentation/categoryDetail/CategoryDetailFragment.kt
+++ features/category/src/main/java/com/hugh/category/presentation/categoryDetail/CategoryDetailFragment.kt
@@ -3,6 +3,8 @@ package com.hugh.category.presentation.categoryDetail
 import android.os.Bundle
 import android.view.View
 import androidx.core.view.isVisible
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -18,8 +20,9 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class CategoryDetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.fragment_detail) {
+class CategoryDetailFragment : Fragment(R.layout.fragment_detail) {
 
+    private lateinit var binding: FragmentDetailBinding
     private val categoryDetailViewModel: CategoryDetailViewModel by viewModels()
 
     private val categoryAdapter = CategoryDetailPagingAdapter()
@@ -27,9 +30,11 @@ class CategoryDetailFragment : BaseFragment<FragmentDetailBinding>(R.layout.frag
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding = DataBindingUtil.bind(view)!!
+
         binding.pagingRecyclerView.apply {
             adapter = categoryAdapter
-            addItemDecoration(GridSpacingItemDecoration(1,30))
+            addItemDecoration(GridSpacingItemDecoration(1, 30))
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
